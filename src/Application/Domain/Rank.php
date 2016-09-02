560b3e95f5501ec5a25803734e0686f56e2055b2
techbot <djtechbot@gmail.com>
2016-09-02 21:41:17 +0200

    get Face value

--- src/Application/Domain/Rank.php
+++ src/Application/Domain/Rank.php
@@ -12,37 +12,43 @@ class Rank
     public function getInt()
     {
         $int = $this->rank;
-
         if ($int == 'T') {
-
             return 10;
-
         }
-
         if ($int == 'J') {
-
             return 11;
-
         }
-
         if ($int == 'Q') {
-
             return 12;
-
         }
-
         if ($int == 'K') {
-
             return 13;
-
         }
-
         if ($int == 'A') {
-
             return 14;
-
         }
         return $int;
+    }
+
+    public function getFace()
+    {
+        $card = $this->rank;
+        if ($card == 14) {
+            return 'A';
+        }
+        if ($card == 13) {
+            return 'K';
+        }
+        if ($card == 12) {
+            return 'Q';
+        }
+        if ($card == 11) {
+            return 'J';
+        }
+        if ($card == 10) {
+            return 'T';
+        }
+        return $card;
     }
 
     public function __construct($rank)
